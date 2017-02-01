e212cc5e9960aab01c8aa873ede6247046c61fa0
Alan Gutierrez <alan@prettyrobots.com>
2017-02-01 12:22:08 -0600

    Skip confusion in the responder.

--- basin.responder.js
+++ basin.responder.js
@@ -8,22 +8,26 @@ function Responder (object) {
 }
 
 Responder.prototype.enqueue = cadence(function (async, envelope) {
-    async(function () {
-        var body = envelope
-        while (typeof body == 'object' && body.type == 'conduit') {
-            body = body.body
-        }
-        this._object.request(body, async())
-    }, function (response) {
-        if (envelope.from != null) {
-            this.responses.enqueue({
-                type: 'conduit',
-                cookie: coalesce(envelope.cookie),
-                to: envelope.from,
-                body: coalesce(response)
-            }, async())
-        }
-    })
+    if (envelope == null) {
+        // TODO Not sure what to do.
+    } else {
+        async(function () {
+            var body = envelope
+            while (typeof body == 'object' && body.type == 'conduit') {
+                body = body.body
+            }
+            this._object.request(body, async())
+        }, function (response) {
+            if (envelope.from != null) {
+                this.responses.enqueue({
+                    type: 'conduit',
+                    cookie: coalesce(envelope.cookie),
+                    to: envelope.from,
+                    body: coalesce(response)
+                }, async())
+            }
+        })
+    }
 })
 
 module.exports = Responder

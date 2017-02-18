1354251c9d8c687e82097f1da0e7b6b84d41d0e9
Alan Gutierrez <alan@prettyrobots.com>
2017-02-18 01:26:47 -0600

    Remove `prefixer.js`.

--- t/prefixer.t.js
+++
@@ -1,7 +0,0 @@
-require('proof/redux')(1, prove)
-
-function prove (assert) {
-    var Prefixer = require('../prefixer')
-    var prefixer = new Prefixer('$')
-    assert(prefixer.map({ cookie: 'submit' }), '$submit', 'map')
-}

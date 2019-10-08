5eded0a3080b7da63a4a95412a9e7bc50b7e7d70
Alan Gutierrez <alan@prettyrobots.com>
2019-10-08 02:31:48 -0500

    Remove older transport concepts.
    Closes #220.

--- test/timeout.t.js
+++
@@ -1,7 +0,0 @@
-require('proof')(2, prove)
-
-function prove (okay) {
-    var Timeout = require('../timeout')
-    okay(Timeout(1000, [ 'x' ]), 1000, 'default')
-    okay(Timeout(1000, [ 2000 ]), 2000, 'specified')
-}
